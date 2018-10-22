8a5cc1c395d2760bf68b6df2eea3f76a1e61a4b8
Matt <7395051@gmail.com>
2018-10-22 15:49:25 -0500

    Added the camera class and integrated it into the graphics class. For now it is set up yet unutilized.

--- 04 Drawing/graphicsclass.cpp
+++ 04 Drawing/graphicsclass.cpp
@@ -6,6 +6,7 @@
 
 GraphicsClass::GraphicsClass()
 {
+	m_Camera = nullptr;
 	m_Direct3D = nullptr;
 }
 
@@ -24,6 +25,17 @@ bool GraphicsClass::Initialize(int screenHeight, int screenWidth, HWND hwnd)
 {
 	bool result;
 
+
+	// Create the camera object.
+	m_Camera = new CameraClass;
+	if (!m_Camera)
+	{
+		return false;
+	}
+
+	// Set the initial parameters of the camera.
+	m_Camera->SetPosition(0.0f, 0.0f, -5.0f);
+	m_Camera->SetLookDirection(0.0f, 0.0f, 1.0f);
 
 	// Create the Direct3D object.
 	m_Direct3D = new D3DClass;
@@ -54,6 +66,13 @@ void GraphicsClass::Shutdown()
 		m_Direct3D = nullptr;
 	}
 
+	// Release the camera object.
+	if (m_Camera)
+	{
+		delete m_Camera;
+		m_Camera = nullptr;
+	}
+
 	return;
 }
 
@@ -78,6 +97,9 @@ bool GraphicsClass::Render()
 {
 	bool result;
 
+
+	// Generate the view matrix based on the camera's position.
+	m_Camera->Render();
 
 	// Use the Direct3D object to render the scene.
 	result = m_Direct3D->Render(0.2f, 0.2f, 0.2f, 1.0f);
